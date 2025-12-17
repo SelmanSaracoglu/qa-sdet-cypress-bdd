a2cb3310c814de1f0a1710869f6dfcd3266c5ece
SelmanSaracoglu <selman.saracoglu@outlook.com>
2025-12-17 14:54:43 +0100

    Initialer Commit: Projekt-Setup mit Cypress und Cucumber

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+
+module.exports = defineConfig({
+    e2e: {
+        specPattern: "cypress/e2e/features/**/*.feature",
+        baseUrl: "https://katalon-demo-cura.herokuapp.com",
+        chromeWebSecurity: false,
+        video: false,
+        screenshotOnRunFailure: true,
+        async setupNodeEvents(on, config) {
+            await preprocessor.addCucumberPreprocessorPlugin(on, config);
+            on(
+                "file:preprocessor",
+                createBundler({
+                    plugins: [createEsbuildPlugin.default(config)],
+                })
+            );
+            return config;
+        },
+    },
+});
